32065f0dc7aca96bf41f5f9ede74f3c231be9a63
Aejera972 <ydr.car.972@gmail.com>
2026-05-19 16:18:40 +0200

    This time I'll finish it

--- script.js
+++ script.js
@@ -9,16 +9,63 @@ function getData() {
     .then((data) => {
       /// EXAM: COMPLÉTEZ LE CODE ICI !
       console.log(data);
-      
+
 
       // TODO 1: REMPLIR LE HEADER
+      let h1 = document.getElementById("nom-journal");
+      console.log(h1);
+
+      let nomJournal = data.journal.nomJournal;
+      console.log(nomJournal);
+
+      h1.textContent = nomJournal;
+
+      let phraseA = document.getElementById("phrase-accroche");
+      console.log(phraseA);
+
+      let phraseAccroche = data.journal.phraseAccroche;
+      console.log(phraseAccroche);
+
+      phraseA.textContent = phraseAccroche;
+
 
       // TODO 2: REMPLIR LA NAVIGATION
 
+
+
+
+
+
+
+
+
+
+
+
+
       // TODO 3: REMPLIR L'ARTICLE PRINCIPAL
 
-      // TODO 4: REMPLIR LA GRILLE D'ARTICLES
 
+
+
+
+
+
+
+      // TODO 4: REMPLIR LA GRILLE D'ARTICLES
+`articles-grid`
+array.forEach(ij => {
+  <div class="">
+        <img src="${}" alt="${} ${}">
+        <p>${}</p>
+        <p>${}</p>
+        <p>${}</p>
+      </div> 
+});
+
+
+
+      
       // TODO 5: REMPLIR LES THEMES
 
       // TODO 6: REMPLIR LES AUTEURS
